629320a67d17467c74efa1829b64235235af95b9
larswarnken <lars.warnken@protonmail.com>
2020-10-23 12:59:04 +0200

    Update main.py

--- twitchbot one file working/main.py
+++ twitchbot one file working/main.py
@@ -123,6 +123,7 @@ while run:
                 send_message(s, message)
 
         # disturbing 1234 emotes
-        if bool(re.search(r'^[^kiando]\w+1\s+\w+2.*', message)):
-            send_message(s, user + ' nice try :)')
-            break
+        if bool(re.search(r'^\w+1\s+\w+2.*', message)):
+            if 'kiando' not in message:
+                send_message(s, user + ' nice try :)')
+                break
